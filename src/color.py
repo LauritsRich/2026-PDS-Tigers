2745dda3bca17ccb7b79375f2bbfca946ff247ac
Dumitru-dev-44 <pyblueprint@gmail.com>
2026-03-22 12:32:10 +0100

    add color feature extraction, version 1

---
+++ src/color.py
@@ -0,0 +1,235 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import cv2
+from skimage.segmentation import slic
+from statistics import variance, stdev
+from skimage.color import rgb2hsv
+from scipy.stats import circmean, circvar, circstd
+from math import sqrt, floor, ceil, nan
+
+
+
+
+
+
+data_path = '../data/'
+def load_image_and_mask(image_id, data_path=data_path):
+    '''
+    Docstring for load_image
+    
+    This function takes an image id, returns the array corresponding to the image and the mask. 
+    It also removes the transparency from the array
+    '''
+    
+    img_path = data_path + "imgs/"
+    mask_path = data_path + "masks/"
+
+    # Load the image/mask
+    file_im = img_path + image_id
+    file_mask = (mask_path + image_id).replace(".png", "_mask.png")
+    im = plt.imread(file_im)
+    mask = plt.imread(file_mask)
+
+    
+    return im[:, :, :3], mask
+
+
+def slic_segmentation(image, mask, n_segments = 10, compactness = 0.1):
+    '''Get color segments of lesion from SLIC algorithm.
+    Optional argument n_segments (default 10) defines desired amount of segments.
+    Optional argument compactness (default 0.1) defines balance between color
+    and position.
+
+    Args:
+        image (numpy.ndarray): image to segment
+        mask (numpy.ndarray):  image mask
+        n_segments (int, optional): desired amount of segments
+        compactness (float, optional): compactness score, decides balance between
+            color and and position
+
+    Returns:
+        slic_segments (numpy.ndarray): SLIC color segments.
+    '''
+    slic_segments = slic(image,
+                    n_segments = n_segments,
+                    compactness = compactness,
+                    sigma = 1,
+                    mask = mask,
+                    start_label = 1,
+                    channel_axis = 2)
+
+    return slic_segments
+
+def get_rgb_means(image, slic_segments):
+    '''Get mean RGB values for each segment in a SLIC segmented image.
+
+    Args:
+        image (numpy.ndarray): original image
+        slic_segments (numpy.ndarray): SLIC segmentation
+
+    Returns:
+        rgb_means (list): RGB mean values for each segment.
+    '''
+
+    max_segment_id = np.unique(slic_segments)[-1]
+
+    rgb_means = []
+    for i in range(1, max_segment_id + 1):
+
+        #Create masked image where only specific segment is active
+        segment = image.copy()
+        segment[slic_segments != i] = -1
+
+        #Get average RGB values from segment
+        rgb_mean = np.mean(segment, axis = (0, 1), where = (segment != -1))
+
+        rgb_means.append(rgb_mean)
+
+    return rgb_means
+
+def rgb_var(image, slic_segments):
+    '''Get variance of RGB means for each segment in
+    SLIC segmentation in red, green and blue channels
+
+    Args:
+        image (numpy.ndarray): image to compute color variance for
+        slic_segments (numpy.ndarray): array containing SLIC segmentation
+
+    Returns:
+        red_var (float): variance in red channel segment means
+        green_var (float): variance in green channel segment means
+        blue_var (float): variance in green channel segment means.
+    '''
+
+    # If there is only 1 slic segment, return (0, 0, 0)
+    if len(np.unique(slic_segments)) == 2: # Use 2 since slic_segments also has 0 for area outside mask
+        return 0, 0, 0
+
+    rgb_means = get_rgb_means(image, slic_segments)
+    n = len(rgb_means) # Amount of segments, used later to compute variance
+
+    # Seperate and collect channel means together in lists
+    red = []
+    green = []
+    blue = []
+    for rgb_mean in rgb_means:
+        red.append(rgb_mean[0])
+        green.append(rgb_mean[1])
+        blue.append(rgb_mean[2])
+
+    # Compute variance for each channel seperately
+    red_var = variance(red, sum(red)/n)
+    green_var = variance(green, sum(green)/n)
+    blue_var = variance(blue, sum(blue)/n)
+
+    return red_var, green_var, blue_var
+
+def get_hsv_means(image, slic_segments):
+    '''Get mean HSV values for each segment in a SLIC segmented image.
+
+    Args:
+        image (numpy.ndarray): original image
+        slic_segments (numpy.ndarray): SLIC segmentation
+
+    Returns:
+        hsv_means (list): HSV mean values for each segment.
+    '''
+
+    hsv_image = rgb2hsv(image)
+
+    max_segment_id = np.unique(slic_segments)[-1]
+
+    hsv_means = []
+    for i in range(1, max_segment_id + 1):
+
+        # Create masked image where only specific segment is active
+        segment = hsv_image.copy()
+        segment[slic_segments != i] = nan
+
+        #Get average HSV values from segment
+        hue_mean = circmean(segment[:, :, 0], high=1, low=0, nan_policy='omit') # Compute circular hue mean
+        sat_mean = np.mean(segment[:, :, 1], where = (slic_segments == i)) # Compute saturation mean
+        val_mean = np.mean(segment[:, :, 2], where = (slic_segments == i)) # Compute value mean
+
+        hsv_mean = np.asarray([hue_mean, sat_mean, val_mean])
+
+        hsv_means.append(hsv_mean)
+
+    return hsv_means
+
+def hsv_var(image, slic_segments):
+    '''Get variance of HSV means for each segment in
+    SLIC segmentation in hue, saturation and value channels
+
+    Args:
+        image (numpy.ndarray): image to compute color variance for
+        slic_segments (numpy.ndarray): array containing SLIC segmentation
+
+    Returns:
+        hue_var (float): variance in hue channel segment means
+        sat_var (float): variance in saturation channel segment means
+        val_var (float): variance in value channel segment means.
+    '''
+
+    # If there is only 1 slic segment, return (0, 0, 0)
+    if len(np.unique(slic_segments)) == 2: # Use 2 since slic_segments also has 0 marking for area outside mask
+        return 0, 0, 0
+
+    hsv_means = get_hsv_means(image, slic_segments)
+    n = len(hsv_means) # Amount of segments, used later to compute variance
+
+    # Seperate and collect channel means together in lists
+    hue = []
+    sat = []
+    val = []
+    for hsv_mean in hsv_means:
+        hue.append(hsv_mean[0])
+        sat.append(hsv_mean[1])
+        val.append(hsv_mean[2])
+
+    # Compute variance for each channel seperately
+    hue_var = circvar(hue, high=1, low=0)
+    sat_var = variance(sat, sum(sat)/n)
+    val_var = variance(val, sum(val)/n)
+
+    return hue_var, sat_var, val_var
+
+def circular_max_min(hue_means):
+
+    '''Get the difference in means between max hue and min hue. Takes into account that hue is circular!!!'''
+    hue = np.array(hue_means)
+
+    diff = np.abs(hue[:, None] - hue[None, :])
+
+    circular_diff = np.minimum(diff, 1 - diff)
+
+    return np.max(circular_diff)
+
+
+#### Combined function
+def color_features_extraction(image_id):
+    image, mask = load_image_and_mask(image_id)
+    slic_segments = slic_segmentation(image, mask)
+
+    r_var, g_var, b_var = rgb_var(image, slic_segments)
+
+    h_var, s_var, v_var = hsv_var(image, slic_segments)
+
+
+    hsv_means = get_hsv_means(image, slic_segments)
+    hue_means = [hsv[0] for hsv in hsv_means]
+
+    circular_max_min_ = circular_max_min(hue_means)
+
+    return [r_var, g_var, b_var, h_var, s_var, v_var, circular_max_min_]
+
+
+    
+
+
+
+
+
+
