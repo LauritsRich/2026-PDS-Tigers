50f48b80131774fad60b737a010854872411b23f
Dumitru-dev-44 <pyblueprint@gmail.com>
2026-04-08 10:23:19 +0200

    update color feature extraction, add CieLab color space mean and variance

--- src/color.py
+++ src/color.py
@@ -1,26 +1,23 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import cv2
 from skimage.segmentation import slic
-from statistics import variance, stdev
-from skimage.color import rgb2hsv
-from scipy.stats import circmean, circvar, circstd
-from math import sqrt, floor, ceil, nan, sin
+from statistics import variance
+from skimage.color import rgb2hsv, rgb2lab
+from scipy.stats import circmean, circvar
+from math import sqrt, nan, sin
 
 
-
+def safe_variance(values):
+    return variance(values) if len(values) > 1 else 0
 
 
 
 data_path = '../data/'
 def load_image_and_mask(image_id, data_path=data_path):
-    '''
-    Docstring for load_image
-    
-    This function takes an image id, returns the array corresponding to the image and the mask. 
-    It also removes the transparency from the array
+    ''' Get the array corresponding to the image and the mask. 
+    Remove transparency channel from the image array.   
+
     '''
     
     img_path = data_path + "imgs/"
@@ -32,7 +29,7 @@ def load_image_and_mask(image_id, data_path=data_path):
     im = plt.imread(file_im)
     mask = plt.imread(file_mask)
 
-    
+    # Get rid of the transparency channel
     return im[:, :, :3], mask
 
 
@@ -83,7 +80,8 @@ def get_rgb_means(image, slic_segments):
         segment[slic_segments != i] = -1
 
         #Get average RGB values from segment
-        rgb_mean = np.mean(segment, axis = (0, 1), where = (segment != -1))
+        mask_i = slic_segments == i
+        rgb_mean = image[mask_i].mean(axis=0)
 
         rgb_means.append(rgb_mean)
 
@@ -120,9 +118,9 @@ def rgb_var(image, slic_segments):
         blue.append(rgb_mean[2])
 
     # Compute variance for each channel seperately
-    red_var = variance(red, sum(red)/n)
-    green_var = variance(green, sum(green)/n)
-    blue_var = variance(blue, sum(blue)/n)
+    red_var = safe_variance(red)
+    green_var = safe_variance(green)
+    blue_var = safe_variance(blue)
 
     return red_var, green_var, blue_var
 
@@ -149,9 +147,12 @@ def get_hsv_means(image, slic_segments):
         segment[slic_segments != i] = nan
 
         #Get average HSV values from segment
+        mask_i = slic_segments == i
+
+
         hue_mean = circmean(segment[:, :, 0], high=1, low=0, nan_policy='omit') # Compute circular hue mean
-        sat_mean = np.mean(segment[:, :, 1], where = (slic_segments == i)) # Compute saturation mean
-        val_mean = np.mean(segment[:, :, 2], where = (slic_segments == i)) # Compute value mean
+        sat_mean = np.mean(segment[:, :, 1][mask_i]) # Compute saturation mean
+        val_mean = np.mean(segment[:, :, 2][mask_i]) # Compute value mean
 
         hsv_mean = np.asarray([hue_mean, sat_mean, val_mean])
 
@@ -191,14 +192,14 @@ def hsv_var(image, slic_segments):
 
     # Compute variance for each channel seperately
     hue_var = circvar(hue, high=1, low=0)
-    sat_var = variance(sat, sum(sat)/n)
-    val_var = variance(val, sum(val)/n)
+    sat_var = safe_variance(sat)
+    val_var = safe_variance(val)
 
     return hue_var, sat_var, val_var
 
 def circular_max_min(hue_means):
 
-    '''Get the difference in means between max hue and min hue. Takes into account that hue is circular!!!'''
+    '''Get the difference in means between max hue and min hue. Takes into account that hue is circular'''
     hue = np.array(hue_means)
 
     diff = np.abs(hue[:, None] - hue[None, :])
@@ -208,7 +209,50 @@ def circular_max_min(hue_means):
     return np.max(circular_diff)
 
 
-#### Combined function
+##### NEW FEATURE - L*a*b* format-medical usage
+def lab_means_per_segment(image, slic_segments):
+    '''Get mean LAB values for each segment in a SLIC segmented image.'''
+
+    lab_image = rgb2lab(image)
+    segment_ids = np.unique(slic_segments)
+####Remove the background except mask
+    segment_ids = segment_ids[segment_ids != 0]
+
+    lab_means = []
+
+    for seg_id in segment_ids:
+        mask = slic_segments == seg_id
+
+        # Extract pixels belonging to the segment
+        segment_pixels = lab_image[mask]
+
+        # Compute mean LAB
+        lab_mean = segment_pixels.mean(axis=0)
+
+        lab_means.append(lab_mean)
+
+    return lab_means
+
+def lab_mean_and_variation(image, slic_segments):
+    '''Get overall mean and variance for the slic segments'''
+
+    means_per_segment = np.array(lab_means_per_segment(image, slic_segments))
+    L_channel = means_per_segment[:, 0]
+    a_channel = means_per_segment[:, 1]
+    b_channel = means_per_segment[:, 2]
+
+
+    L_mean = np.mean(L_channel)
+    a_mean = np.mean(a_channel)
+    b_mean = np.mean(b_channel)
+
+    L_var = safe_variance(L_channel)
+    a_var = safe_variance(a_channel)
+    b_var = safe_variance(b_channel)
+    return L_mean,a_mean,b_mean, L_var,a_var,b_var
+
+
+#### Combined function for all possible feature combinations
 def color_features_extraction(image_id):
     image, mask = load_image_and_mask(image_id)
     slic_segments = slic_segmentation(image, mask)
@@ -218,9 +262,9 @@ def color_features_extraction(image_id):
     h_var, s_var, v_var = hsv_var(image, slic_segments)
 
     hsv_means = get_hsv_means(image, slic_segments)
-    hue_means = [hsv[0] for hsv in hsv_means]
-    v_means = [hsv[1] for hsv in hsv_means]
-    s_means = [hsv[2] for hsv in hsv_means]
+    hue_means = np.array([hsv[0] for hsv in hsv_means])
+    s_means = [hsv[1] for hsv in hsv_means]
+    v_means = [hsv[2] for hsv in hsv_means]
 
     rgb_means = get_rgb_means(image, slic_segments)
     r_means = [rgb[0] for rgb in rgb_means]
@@ -229,14 +273,21 @@ def color_features_extraction(image_id):
 
     circular_max_min_h = circular_max_min(hue_means)
     ##combine all variances together
+
+    ## Magnitude of variance
     rgb_var_mag = np.sqrt(r_var**2 + g_var**2 + b_var**2)
     hsv_var_mag = np.sqrt(h_var**2 + s_var**2 + v_var**2)
 
-
+    ## Mean of variance
     rgb_var_mean = np.mean([r_var, g_var, b_var])
     hsv_var_mean = np.mean([h_var, s_var, v_var])
 
-    h_sin = np.sin(np.mean(hue_means))### hue is circular thats why we use trigonometric function
+### hue is circular thats why we use trigonometric functions as below:
+    h_cos = np.cos(2 * np.pi * hue_means)
+    h_sin = np.sin(2 * np.pi * hue_means)
+
+    mean_angle_h = np.arctan2(np.mean(h_sin), np.mean(h_cos))
+
     v_value = np.mean(v_means)
     s_value = np.mean(s_means)
 
@@ -244,9 +295,12 @@ def color_features_extraction(image_id):
     g_value = np.mean(g_means)
     b_value = np.mean(b_means)
 
+    ### LAB thingies
+    Ls_value, as_value, bs_value, Ls_var, as_var, bs_var = lab_mean_and_variation(image, slic_segments)
+
 
     #### keep the hue distance, magnitude of the rgb and hsv variances and the circular; return all posible features to see the ones that affect the most 
-    return [h_sin, s_value, v_value, r_value, g_value, b_value, h_var, s_var, v_var, r_var, g_var, b_var, hsv_var_mean, rgb_var_mean, hsv_var_mag, rgb_var_mag, circular_max_min_h]
+    return [Ls_value, as_value, bs_value, mean_angle_h, s_value, v_value, r_value, g_value, b_value,Ls_var, as_var, bs_var, h_var, s_var, v_var, r_var, g_var, b_var, hsv_var_mean, rgb_var_mean, hsv_var_mag, rgb_var_mag, circular_max_min_h]
 
 
     
